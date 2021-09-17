c24e572625e377d50bba33edd5eec1b663c3fded
BlackCosmos7 <74289814+BlackCosmos7@users.noreply.github.com>
2021-09-17 12:47:47 +0300

    Add files via upload

---
+++ 1909My/index.js
@@ -0,0 +1,19 @@
+class Person {
+    constructor(surname, name, gender, birthdate) {
+        this.surname =  surname;
+        this.name = name;
+        this.gender = gender;
+        this.birthdate = birthdate;
+    }
+}
+data.split(';;;;')
+let arr =
+str.split([separator[limit]])
+// let Person = require("fs");
+//    const cb = function() {
+//    console.log("tables.csv");
+// }
+
+// let text;
+// Person.readFile("tables.csv", 
+// { encoding: "utf-8"}, cb); 
